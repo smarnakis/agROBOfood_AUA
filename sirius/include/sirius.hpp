29a9a72f2766e6a6c2ae2af6332eb3b39f22127f
Aris Smarnakis <aris.smarnakis@gmail.com>
2020-05-08 12:57:59 +0300

    #10 stable full-stop safety

--- sirius/include/sirius.hpp
+++ sirius/include/sirius.hpp
@@ -4,17 +4,21 @@
 /* ROS headers */
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <ros/console.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/approximate_time.h>
+/* ROS MSGs Headers */
+#include <sensor_msgs/LaserScan.h>
+#include <geometry_msgs/Twist.h>
 #include <std_msgs/String.h>
 #include <std_msgs/Bool.h>
-
+#include <geometry_msgs/PointStamped.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
 
 #include <robot_localization/navsat_conversions.h>
-#include <geometry_msgs/PointStamped.h>
+
 
 /* TF headers */
 #include <tf/transform_broadcaster.h>
@@ -66,4 +70,38 @@ geometry_msgs::PointStamped UTMtoMapPoint(geometry_msgs::PointStamped UTM_input)
 //Create moveBaseGoal msg for the move base action client.
 move_base_msgs::MoveBaseGoal buildGoal(geometry_msgs::PointStamped map_point, geometry_msgs::PointStamped map_next, bool last_point);
 
+
+/* CallBack function headers */
+void safetyCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
+
+/* Classes */
+class safety
+{
+private:
+	ros::Publisher pub_;
+	ros::Subscriber sub_;
+public:
+	safety(ros::NodeHandle n){
+		pub_ = n.advertise<geometry_msgs::Twist>("/emergency_cmd",10);
+		sub_ = n.subscribe("/scan",10,&safety::callback,this);
+	}
+	void callback(const sensor_msgs::LaserScan::ConstPtr& msg){
+	  std::vector<float> ran;
+	  ran = msg->ranges;
+	  geometry_msgs::Twist stop_vel;
+	  stop_vel.linear.x = 0.0;
+	  stop_vel.linear.y = 0.0;
+	  stop_vel.linear.z = 0.0;
+	  stop_vel.angular.x = 0.0;
+	  stop_vel.angular.y = 0.0;
+	  stop_vel.angular.z = 0.0;
+	  if ( std::all_of(ran.begin(),ran.end(),[](float i){return i>1.5;}) ){
+	    ROS_INFO("ALL GOOD");
+	  }
+	  else{
+	  	pub_.publish(stop_vel);
+	    ROS_WARN("SIRIUS IN COLLISION COURSE!!!!");
+	  }
+	}
+};
 #endif
--- sirius/src/safety.cpp
+++ sirius/src/safety.cpp
@@ -1,35 +1,11 @@
-#include <ros/ros.h>
-#include <sensor_msgs/LaserScan.h>
-#include <ros/console.h>
-
-
-void safetyCallback(const sensor_msgs::LaserScan::ConstPtr& msg){
-/*	float max_range = msg->range_max;
-  float angle_min,angle_max,angle_inc;
-  int measurements;
-  angle_min = msg->angle_min;
-  angle_max = msg->angle_max;
-  angle_inc = msg->angle_increment;
-  measurements = (angle_max - angle_min) / angle_inc;
-  float ranges[measurements];*/
-  std::vector<float> ran;
-  ran = msg->ranges;
-  if ( std::all_of(ran.begin(),ran.end(),[](float i){return i>1;}) ){
-    ROS_INFO("ALL GOOD");
-  }
-  else{
-    ROS_WARN("SIRIUS IN COLLISION COURSE!!!!");
-  }
-/*
-  ROS_INFO("%f | %f | %f\n",angle_min,angle_max,angle_inc);
-  ROS_INFO("%f\n",ran[100]);*/
-}
+#include "sirius.hpp"
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "safety_node");
 
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("/scan",10,safetyCallback);
+
+  safety safety_obj(nh);
 
   ros::spin();
 
