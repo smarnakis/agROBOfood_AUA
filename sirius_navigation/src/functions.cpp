2ef9dd84b15192fa005ca265dfb6e3d22e89b6f5
Aris Smarnakis <aris.smarnakis@gmail.com>
2020-07-30 10:52:18 +0300

    #36 planner_3 with start trigger

--- sirius_navigation/src/functions.cpp
+++ sirius_navigation/src/functions.cpp
@@ -10,160 +10,120 @@ double latiGoal, longiGoal, latiNext, longiNext,latiCurr,longiCurr;
 std::string utm_zone;
 std::string path_local, path_abs;
 
+
 int countWaypointsInFile(std::string path_local)
 {
-    path_abs = ros::package::getPath("sirius_navigation") + path_local;
-    std::ifstream fileCount(path_abs.c_str());
-    if(fileCount.is_open())
-    {
-        double lati = 0;
-        while(!fileCount.eof())
-        {
-            fileCount >> lati;
-            ++count;
-        }
-        count = count - 1;
-        numWaypoints = count / 2;
-        ROS_INFO("%.0f map coordinate waypoints were read", numWaypoints);
-        fileCount.close();
-    }
-    else
-    {
-        std::cout << "Unable to open waypoint file" << std::endl;
-        ROS_ERROR("Unable to open waypoint file");
-    }
-    return numWaypoints;
+  path_abs = ros::package::getPath("sirius_navigation") + path_local;
+  std::ifstream fileCount(path_abs.c_str());
+  if(fileCount.is_open())
+  {
+      double lati = 0;
+      while(!fileCount.eof())
+      {
+          fileCount >> lati;
+          ++count;
+      }
+      count = count - 1;
+      numWaypoints = count / 2;
+      ROS_INFO("%.0f map coordinate waypoints were read", numWaypoints);
+      fileCount.close();
+  }
+  else
+  {
+      std::cout << "Unable to open waypoint file" << std::endl;
+      ROS_ERROR("Unable to open waypoint file");
+  }
+  return numWaypoints;
 }
 
 std::vector <std::pair<double, double>> getWaypoints(std::string path_local)
 {
-    double lati = 0, longi = 0;
+  double lati = 0, longi = 0;
 
-    path_abs = ros::package::getPath("sirius_navigation") + path_local;
-    std::ifstream fileRead(path_abs.c_str());
-    for(int i = 0; i < numWaypoints; i++)
-    {
-        fileRead >> lati;
-        fileRead >> longi;
-        waypointVect.push_back(std::make_pair(lati, longi));
-    }
-    fileRead.close();
+  path_abs = ros::package::getPath("sirius_navigation") + path_local;
+  std::ifstream fileRead(path_abs.c_str());
+  for(int i = 0; i < numWaypoints; i++)
+  {
+      fileRead >> lati;
+      fileRead >> longi;
+      waypointVect.push_back(std::make_pair(lati, longi));
+  }
+  fileRead.close();
 
-    //Outputting vector
-    ROS_INFO("The following Map coordinate Waypoints have been set:");
-    for(std::vector < std::pair < double, double >> ::iterator iterDisp = waypointVect.begin(); iterDisp != waypointVect.end();
-    iterDisp++)
-    {
-        ROS_INFO("%.9g %.9g", iterDisp->first, iterDisp->second);
-    }
-    return waypointVect;
+  //Outputting vector
+  ROS_INFO("The following Map coordinate Waypoints have been set:");
+  for(std::vector < std::pair < double, double >> ::iterator iterDisp = waypointVect.begin(); iterDisp != waypointVect.end();
+  iterDisp++)
+  {
+      ROS_INFO("%.9g %.9g", iterDisp->first, iterDisp->second);
+  }
+  return waypointVect;
 }
 
 geometry_msgs::PointStamped latLongtoUTM(double lati_input, double longi_input)
 {
-    double utm_x = 0, utm_y = 0;
-    geometry_msgs::PointStamped UTM_point_output;
+  double utm_x = 0, utm_y = 0;
+  geometry_msgs::PointStamped UTM_point_output;
 
-    //convert lat/long to utm
-    RobotLocalization::NavsatConversions::LLtoUTM(lati_input, longi_input, utm_y, utm_x, utm_zone);
+  //convert lat/long to utm
+  RobotLocalization::NavsatConversions::LLtoUTM(lati_input, longi_input, utm_y, utm_x, utm_zone);
 
-    //Construct UTM_point and map_point geometry messages
-    UTM_point_output.header.frame_id = "utm";
-    UTM_point_output.header.stamp = ros::Time(0);
-    UTM_point_output.point.x = utm_x;
-    UTM_point_output.point.y = utm_y;
-    UTM_point_output.point.z = 0;
+  //Construct UTM_point and map_point geometry messages
+  UTM_point_output.header.frame_id = "utm";
+  UTM_point_output.header.stamp = ros::Time(0);
+  UTM_point_output.point.x = utm_x;
+  UTM_point_output.point.y = utm_y;
+  UTM_point_output.point.z = 0;
 
-    return UTM_point_output;
+  return UTM_point_output;
 }
 
 geometry_msgs::PointStamped UTMtoMapPoint(geometry_msgs::PointStamped UTM_input)
 {
-    geometry_msgs::PointStamped map_point_output;
-    bool notDone = true;
-    tf::TransformListener listener; //create transformlistener object called listener
-    ros::Time time_now = ros::Time::now();
-    while(notDone)
+  geometry_msgs::PointStamped map_point_output;
+  bool notDone = true;
+  tf::TransformListener listener; //create transformlistener object called listener
+  ros::Time time_now = ros::Time::now();
+  while(notDone)
+  {
+    try
     {
-        try
-        {
-            UTM_point.header.stamp = ros::Time::now();
-            listener.waitForTransform("odom", "utm", time_now, ros::Duration(3.0));
-            listener.transformPoint("odom", UTM_input, map_point_output);
-            notDone = false;
-        }
-        catch (tf::TransformException& ex)
-        {
-            ROS_WARN("%s", ex.what());
-            ros::Duration(0.01).sleep();
-            //return;
-        }
+        UTM_point.header.stamp = ros::Time::now();
+        listener.waitForTransform("odom", "utm", time_now, ros::Duration(3.0));
+        listener.transformPoint("odom", UTM_input, map_point_output);
+        notDone = false;
     }
-    return map_point_output;
+    catch (tf::TransformException& ex)
+    {
+        ROS_WARN("%s", ex.what());
+        ros::Duration(0.01).sleep();
+        //return;
+    }
+  }
+  return map_point_output;
 }
 
 move_base_msgs::MoveBaseGoal buildGoal(geometry_msgs::PointStamped map_point, geometry_msgs::PointStamped map_next, bool last_point)
 {
-    move_base_msgs::MoveBaseGoal goal;
+  move_base_msgs::MoveBaseGoal goal;
 
-    //Specify what frame we want the goal to be published in
-    goal.target_pose.header.frame_id = "odom";
-    goal.target_pose.header.stamp = ros::Time::now();
+  //Specify what frame we want the goal to be published in
+  goal.target_pose.header.frame_id = "odom";
+  goal.target_pose.header.stamp = ros::Time::now();
 
-    // Specify x and y goal
-    goal.target_pose.pose.position.x = map_point.point.x; //specify x goal
-    goal.target_pose.pose.position.y = map_point.point.y; //specify y goal
+  // Specify x and y goal
+  goal.target_pose.pose.position.x = map_point.point.x; //specify x goal
+  goal.target_pose.pose.position.y = map_point.point.y; //specify y goal
 
-    // Specify heading goal using current goal and next goal (point robot towards its next goal once it has achieved its current goal)
-    if(last_point == false)
-    {
-        tf::Matrix3x3 rot_euler;
-        tf::Quaternion rot_quat;
-
-        // Calculate quaternion
-        float x_curr = map_point.point.x, y_curr = map_point.point.y; // set current coords.
-        float x_next = map_next.point.x, y_next = map_next.point.y; // set coords. of next waypoint
-        float delta_x = x_next - x_curr, delta_y = y_next - y_curr;   // change in coords.
-        float yaw_curr = 0, pitch_curr = 0, roll_curr = 0;
-        yaw_curr = atan2(delta_y, delta_x);
-
-        // Specify quaternions
-        rot_euler.setEulerYPR(yaw_curr, pitch_curr, roll_curr);
-        rot_euler.getRotation(rot_quat);
-
-        goal.target_pose.pose.orientation.x = rot_quat.getX();
-        goal.target_pose.pose.orientation.y = rot_quat.getY();
-        goal.target_pose.pose.orientation.z = rot_quat.getZ();
-        goal.target_pose.pose.orientation.w = rot_quat.getW();
-    }
-    else
-    {
-        goal.target_pose.pose.orientation.w = 1.0;
-    }
-
-    return goal;
-}
-
-
-move_base_msgs::MoveBaseGoal buildGoal_2(geometry_msgs::PointStamped map_point_curr, geometry_msgs::PointStamped map_goal)
-{
-    move_base_msgs::MoveBaseGoal goal;
-
-    //Specify what frame we want the goal to be published in
-    goal.target_pose.header.frame_id = "odom";
-    goal.target_pose.header.stamp = ros::Time::now();
-
-    // Specify x and y goal
-    goal.target_pose.pose.position.x = map_goal.point.x; //specify x goal
-    goal.target_pose.pose.position.y = map_goal.point.y; //specify y goal
-
-    // Specify heading goal using current goal and next goal (point robot towards its next goal once it has achieved its current goal)
+  // Specify heading goal using current goal and next goal (point robot towards its next goal once it has achieved its current goal)
+  if(last_point == false)
+  {
     tf::Matrix3x3 rot_euler;
     tf::Quaternion rot_quat;
 
     // Calculate quaternion
-    float x_curr = map_point_curr.point.x, y_curr = map_point_curr.point.y; // set current coords.
-    float x_next = map_goal.point.x, y_next = map_goal.point.y; // set coords. of next waypoint
+    float x_curr = map_point.point.x, y_curr = map_point.point.y; // set current coords.
+    float x_next = map_next.point.x, y_next = map_next.point.y; // set coords. of next waypoint
     float delta_x = x_next - x_curr, delta_y = y_next - y_curr;   // change in coords.
     float yaw_curr = 0, pitch_curr = 0, roll_curr = 0;
     yaw_curr = atan2(delta_y, delta_x);
@@ -176,72 +136,113 @@ move_base_msgs::MoveBaseGoal buildGoal_2(geometry_msgs::PointStamped map_point_c
     goal.target_pose.pose.orientation.y = rot_quat.getY();
     goal.target_pose.pose.orientation.z = rot_quat.getZ();
     goal.target_pose.pose.orientation.w = rot_quat.getW();
+  }
+  else
+  {
+      goal.target_pose.pose.orientation.w = 1.0;
+  }
 
-    return goal;
+  return goal;
+}
+
+
+move_base_msgs::MoveBaseGoal buildGoal_2(geometry_msgs::PointStamped map_point_curr, geometry_msgs::PointStamped map_goal)
+{
+  move_base_msgs::MoveBaseGoal goal;
+
+  //Specify what frame we want the goal to be published in
+  goal.target_pose.header.frame_id = "odom";
+  goal.target_pose.header.stamp = ros::Time::now();
+
+  // Specify x and y goal
+  goal.target_pose.pose.position.x = map_goal.point.x; //specify x goal
+  goal.target_pose.pose.position.y = map_goal.point.y; //specify y goal
+
+  // Specify heading goal using current goal and next goal (point robot towards its next goal once it has achieved its current goal)
+  tf::Matrix3x3 rot_euler;
+  tf::Quaternion rot_quat;
+
+  // Calculate quaternion
+  float x_curr = map_point_curr.point.x, y_curr = map_point_curr.point.y; // set current coords.
+  float x_next = map_goal.point.x, y_next = map_goal.point.y; // set coords. of next waypoint
+  float delta_x = x_next - x_curr, delta_y = y_next - y_curr;   // change in coords.
+  float yaw_curr = 0, pitch_curr = 0, roll_curr = 0;
+  yaw_curr = atan2(delta_y, delta_x);
+
+  // Specify quaternions
+  rot_euler.setEulerYPR(yaw_curr, pitch_curr, roll_curr);
+  rot_euler.getRotation(rot_quat);
+
+  goal.target_pose.pose.orientation.x = rot_quat.getX();
+  goal.target_pose.pose.orientation.y = rot_quat.getY();
+  goal.target_pose.pose.orientation.z = rot_quat.getZ();
+  goal.target_pose.pose.orientation.w = rot_quat.getW();
+
+  return goal;
 }
 
 
 move_base_msgs::MoveBaseGoal half_rotation()
 {
-    move_base_msgs::MoveBaseGoal goal;
-    tf::Matrix3x3 rot_euler;
-    tf::Quaternion rot_quat;
-    //Specify what frame we want the goal to be published in
-    goal.target_pose.header.frame_id = "base_link";
-    goal.target_pose.header.stamp = ros::Time::now();
-    float yaw_curr = 0, pitch_curr = 0, roll_curr = 0;
-    yaw_curr = 3.14;
+  move_base_msgs::MoveBaseGoal goal;
+  tf::Matrix3x3 rot_euler;
+  tf::Quaternion rot_quat;
+  //Specify what frame we want the goal to be published in
+  goal.target_pose.header.frame_id = "base_link";
+  goal.target_pose.header.stamp = ros::Time::now();
+  float yaw_curr = 0, pitch_curr = 0, roll_curr = 0;
+  yaw_curr = 3.14;
 
-    // Specify quaternions
-    rot_euler.setEulerYPR(yaw_curr, pitch_curr, roll_curr);
-    rot_euler.getRotation(rot_quat);
-    // Specify x and y, w goal
-    goal.target_pose.pose.position.x = 0.0; //specify x goal
-    goal.target_pose.pose.position.y = 0.0; //specify y goal
-    // goal.target_pose.pose.orientation.z = 1.0;
-    // goal.target_pose.pose.orientation.w = 0.0;
-    goal.target_pose.pose.orientation.x = rot_quat.getX();
-    goal.target_pose.pose.orientation.y = rot_quat.getY();
-    goal.target_pose.pose.orientation.z = rot_quat.getZ();
-    goal.target_pose.pose.orientation.w = rot_quat.getW();
-    return goal;
+  // Specify quaternions
+  rot_euler.setEulerYPR(yaw_curr, pitch_curr, roll_curr);
+  rot_euler.getRotation(rot_quat);
+  // Specify x and y, w goal
+  goal.target_pose.pose.position.x = 0.0; //specify x goal
+  goal.target_pose.pose.position.y = 0.0; //specify y goal
+  // goal.target_pose.pose.orientation.z = 1.0;
+  // goal.target_pose.pose.orientation.w = 0.0;
+  goal.target_pose.pose.orientation.x = rot_quat.getX();
+  goal.target_pose.pose.orientation.y = rot_quat.getY();
+  goal.target_pose.pose.orientation.z = rot_quat.getZ();
+  goal.target_pose.pose.orientation.w = rot_quat.getW();
+  return goal;
 }
 
 move_base_msgs::MoveBaseGoal simpleGoal(tf::StampedTransform transform)
 {
-    move_base_msgs::MoveBaseGoal goal;
-    tf::Matrix3x3 rot_euler;
-    tf::Quaternion rot_quat;  
-    float X0,Y0,theta,X,Y,yaw_goal,x=1,y=0;
+  move_base_msgs::MoveBaseGoal goal;
+  tf::Matrix3x3 rot_euler;
+  tf::Quaternion rot_quat;  
+  float X0,Y0,theta,X,Y,yaw_goal,x=1,y=0;
 
-    rot_quat = transform.getRotation();
-    X0 = transform.getOrigin().x();
-    Y0 = transform.getOrigin().y();
-    theta = rot_quat.getAngle();
-    
-    ROS_INFO("Origin: X0 = %f,Y0 = %f\n",X0,Y0);
-    ROS_INFO("Rotation: z = %f, w = %f\n",rot_quat.getZ(),rot_quat.getW());
-    ROS_INFO("Base <-- Odom rot angle: theta = %f\n",theta);
+  rot_quat = transform.getRotation();
+  X0 = transform.getOrigin().x();
+  Y0 = transform.getOrigin().y();
+  theta = rot_quat.getAngle();
+  
+  ROS_INFO("Origin: X0 = %f,Y0 = %f\n",X0,Y0);
+  ROS_INFO("Rotation: z = %f, w = %f\n",rot_quat.getZ(),rot_quat.getW());
+  ROS_INFO("Base <-- Odom rot angle: theta = %f\n",theta);
 
-    X = x*cos(theta) - y*sin(theta) + X0;
-    Y = y*cos(theta) + x*sin(theta) + Y0;
-    yaw_goal = atan2(Y-Y0,X-X0);
+  X = x*cos(theta) - y*sin(theta) + X0;
+  Y = y*cos(theta) + x*sin(theta) + Y0;
+  yaw_goal = atan2(Y-Y0,X-X0);
 
-    ROS_INFO("GOAL: X = %f,Y = %f\n",X,Y);
-    rot_euler.setEulerYPR(yaw_goal, 0, 0);
-    rot_euler.getRotation(rot_quat);
-    //Specify what frame we want the goal to be published in
-    goal.target_pose.header.frame_id = "odom";
-    goal.target_pose.header.stamp = ros::Time::now();
+  ROS_INFO("GOAL: X = %f,Y = %f\n",X,Y);
+  rot_euler.setEulerYPR(yaw_goal, 0, 0);
+  rot_euler.getRotation(rot_quat);
+  //Specify what frame we want the goal to be published in
+  goal.target_pose.header.frame_id = "odom";
+  goal.target_pose.header.stamp = ros::Time::now();
 
-    // Specify x and y goal
-    goal.target_pose.pose.position.x = X; //specify x goal
-    goal.target_pose.pose.position.y = Y; //specify y goal
-    goal.target_pose.pose.orientation.x = rot_quat.getX();
-    goal.target_pose.pose.orientation.y = rot_quat.getY();
-    goal.target_pose.pose.orientation.z = rot_quat.getZ();
-    goal.target_pose.pose.orientation.w = rot_quat.getW();
+  // Specify x and y goal
+  goal.target_pose.pose.position.x = X; //specify x goal
+  goal.target_pose.pose.position.y = Y; //specify y goal
+  goal.target_pose.pose.orientation.x = rot_quat.getX();
+  goal.target_pose.pose.orientation.y = rot_quat.getY();
+  goal.target_pose.pose.orientation.z = rot_quat.getZ();
+  goal.target_pose.pose.orientation.w = rot_quat.getW();
 /*    goal.target_pose.pose.orientation.w = 1.0;*/
-    
-    return goal;
+  
+  return goal;
 }
---
+++ sirius_navigation/src/gps_planner_3.cpp
@@ -0,0 +1,137 @@
+#include "sirius_navigation.hpp"
+
+typedef actionlib::SimpleActionClient <move_base_msgs::MoveBaseAction>
+MoveBaseClient; //create a type definition for a client called MoveBaseClient
+
+int trig_but1,trig_but2;
+bool trigger = false;
+
+/* Callbacks */
+void resume_motion_callback(const sensor_msgs::Joy::ConstPtr& msg){
+  if(msg->buttons[trig_but1]==1 && msg->buttons[trig_but2]==1)
+  {
+    ROS_WARN("CALLBACK TRIG RECEIVED");
+    trigger = true;
+  }
+  else{
+    trigger = false;
+  }
+}
+/*************/
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "gps_waypoint"); //initiate node called gps_waypoint
+    ros::NodeHandle n;
+    ROS_INFO("Initiated gps_waypoint node");
+    MoveBaseClient ac("/move_base", true);
+
+    //Subscribe to joystick to listen to the motion trigger.
+    ros::Subscriber sub_joy = n.subscribe("/joy_teleop/joy", 1, resume_motion_callback);
+    ros::param::get("/planner/trigger_button1", trig_but1);
+    ros::param::get("/planner/trigger_button2", trig_but2);
+
+
+    // Initiate publisher to send end of node message
+    ros::Publisher pubWaypointNodeEnded = n.advertise<std_msgs::Bool>("/waypoint_following_status", 100);
+
+    //wait for the action server to come up
+    while(!ac.waitForServer(ros::Duration(5.0)))
+    {
+        wait_count++;
+        if(wait_count > 3)
+        {
+            ROS_ERROR("move_base action server did not come up, killing gps_waypoint node...");
+            // Notify joy_launch_control that waypoint following is complete
+            std_msgs::Bool node_ended;
+            node_ended.data = true;
+            pubWaypointNodeEnded.publish(node_ended);
+            ros::shutdown();
+        }
+        ROS_INFO("Waiting for the move_base action server to come up");
+    }
+
+    //Get Longitude and Latitude goals from text file
+
+    //Count number of waypoints
+    ros::param::get("/planner/gps_coordinates_file", path_local);
+    // ROS_INFO(path_local);
+    numWaypoints = countWaypointsInFile(path_local);
+
+    //Reading waypoints from text file and output results
+    waypointVect = getWaypoints(path_local);
+	ROS_WARN("Waypoint vector length = %d",waypointVect.size());
+    // Iterate through vector of waypoints for setting goals
+
+    for(iter = waypointVect.begin(); iter < (waypointVect.end() - 1); iter++)
+    {
+        //Setting goal:
+        latiCurr = iter->first;
+        longiCurr = iter->second;
+        bool final_point = false;
+
+        //set next goal point if not at last waypoint
+        iter++;
+        latiGoal = iter->first;
+        longiGoal = iter->second;
+        iter--;
+
+
+        ROS_INFO("Received Latitude goal:%.8f", latiGoal);
+        ROS_INFO("Received longitude goal:%.8f", longiGoal);
+
+        //Convert lat/long to utm:
+        UTM_point = latLongtoUTM(latiCurr, longiCurr);
+        UTM_next = latLongtoUTM(latiGoal, longiGoal);
+
+        //Transform UTM to map point in odom frame
+        map_point = UTMtoMapPoint(UTM_point);
+        map_next = UTMtoMapPoint(UTM_next);
+
+        //Build goal to send to move_base
+        move_base_msgs::MoveBaseGoal goal = buildGoal_2(map_point, map_next); //initiate a move_base_msg called goal
+
+        ROS_INFO("Sirius waits for motion trigger...");
+        while(!trigger){
+            ros::spinOnce();
+        }
+        trigger = false;
+        ROS_INFO("Motion triggered!! Moving to the next waypoint..");
+
+
+        // Send Goal
+        ROS_INFO("Sending goal");
+        ac.sendGoal(goal); //push goal to move_base node
+
+        //Wait for result
+        ac.waitForResult(); //waiting to see if move_base was able to reach goal
+
+        if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+        {
+            ROS_INFO("Husky has reached its goal!");
+            //switch to next waypoint and repeat
+        }
+        else
+        {
+            ROS_ERROR("Husky was unable to reach its goal. GPS Waypoint unreachable.");
+            ROS_INFO("Exiting node...");
+            // Notify joy_launch_control that waypoint following is complete
+            std_msgs::Bool node_ended;
+            node_ended.data = true;
+            pubWaypointNodeEnded.publish(node_ended);
+            // ros::shutdown();
+        }
+    } // End for loop iterating through waypoint vector
+    
+    ROS_INFO("Husky has reached all of its goals!!!\n");
+    ROS_INFO("Ending node...");
+
+    // Notify joy_launch_control that waypoint following is complete
+    std_msgs::Bool node_ended;
+    node_ended.data = true;
+    pubWaypointNodeEnded.publish(node_ended);
+
+    ros::shutdown();
+    ros::spin();
+    return 0;
+}
